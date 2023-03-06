1f97176cceb44ed6d0a839a8649dee7cb4fbfa72
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-06 16:00:18 -0300

    Create basic operation functions

---
+++ script.js
@@ -0,0 +1,23 @@
+let checker =0;
+let num1 = ``;
+let num2 = ``;
+
+function add(num1, num2){
+    const sum = Number(num1) + Number(num2);
+    return sum;
+}
+
+function subtract(num1, num2){
+    const sum = Number(num1) - Number(num2);
+    return sum;
+}
+
+function multiply(num1, num2){
+    const sum = Number(num1) * Number(num2);
+    return sum;
+}
+
+function divide(num1, num2){
+    const sum = Number(num1) / Number(num2);
+    return sum;
+}
