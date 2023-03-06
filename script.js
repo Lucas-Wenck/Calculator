86b03886f66c116a283cb9e76e82d4dab55e406c
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-06 16:23:28 -0300

    Add event listener for num pad numbers

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let checker =0;
+let checker = 0;
 let num1 = ``;
 let num2 = ``;
+let sum = ``;
+let numEvent = document.getElementsByClassName('numPadNumber');
+
+for(let i = 0; i < numEvent.length; i++){
+    numEvent[i].addEventListener('click', function(){
+        num1 += numEvent[i].innerText;
+        sum = num1;
+        document.getElementById('display-num').innerHTML = `${sum}`;
+    })
+}
 
 function add(num1, num2){
     const sum = Number(num1) + Number(num2);
@@ -19,5 +29,5 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     const sum = Number(num1) / Number(num2);
-    return sum;
+    return sum; 
 }
